d55802eab4fe94f0c177a23b6423206b8c1651b9
zsuuer <38557333+zsuuer@users.noreply.github.com>
2018-05-26 17:58:35 +0800

    无向图求最短路径
    对于无向网，首先先构建一个邻接矩阵arcs来表示无向网中结点与结点之间的连接关系和权重，然后根据dijkstra算法求出某个结点到其余结点的最短路径和经过的结点，dijkstra算法采用的是一步一步求出到某个结点的最短路径的类似贪心算法的方法，先求出某个离源点最近的一个结点，那么这个结点的最短路径就是这个权值，因为通过数学证明可以发现在图中无法找到一条更短的路径到达这个结点，然后根据这个结点的出度去修正其他结点的当前的最短路径，即d【】数组，并将最短路径经过的结点通过parcs【】矩阵保存下来，然后把这个已知的最短路径结点添加到t【】数组，再从d【】数组挑出最短的路径，但忽略已得到最短路径的结点即通过t【】数组来指示。重复这些步骤最后得到到各个结点的最短路径和路过的结点。

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,83 +1,136 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define graphnum 6
 int main()
 {
-	int i;
-	char buffer[1024*1024];		//创建缓冲区；
-	char one_buffer[1]; 
-	int last_f=0; 			//定义文件能够分割成1m的最后的字节位置；	 
-	int last_s;				//如果文件剩余大小不足1M，则算出具体剩余多少字节；
-	char *a[6]={"e:\\cxtest\\testtfboy(1).mp3","e:\\cxtest\\testtfboy(2).mp3","e:\\cxtest\\testtfboy(3).mp3",	
-				"e:\\cxtest\\testtfboy(4).mp3","e:\\cxtest\\testtfboy(5).mp3","e:\\cxtest\\testtfboy(6).mp3"}; //定义分割的文件名 
-	FILE *f=fopen("e:\\tfboy.mp3","rb");	//打开分割文件的数据流 
-   	printf("begin spilt the file tfboy.mp3");
-   	printf("sizeof char%d\n",sizeof(buffer));
-	if(ferror(f))		//如果打开失败，报错； 
-   	{
-   		printf("can not open the file tfboy.mp3 successful");		
-	}
-	else 
-	{
-		for (i=0;i<6;i++)
-		{
-			FILE *f1=fopen(a[i],"wb");
-			if(fread(buffer,sizeof(buffer),1,f)==0)	//如果读取1M字节失败，报错，并计算剩余多少字节，将剩余的部分写入文件； 
-   			{
-   				printf("can not create the file tfboy.mp3 successful\n");
-				printf("i am here %d\n",ftell(f));
-				printf("i am here %d\n",last_f);
-				fseek(f,last_f,SEEK_SET);
-				printf("i am here %d\n",ftell(f));
-				printf("last_s %d\n",last_f);
-				while(fread(one_buffer,sizeof(one_buffer),1,f)!=0)
-				{
-					fseek(f,-1,SEEK_CUR);	
-					fread(one_buffer,sizeof(one_buffer),1,f);
-					fwrite(one_buffer,sizeof(one_buffer),1,f1);
-					
-				}	
-				fclose(f1);
-				printf("i am here %d\n",ftell(f));
-			}		
-			else	//如果成功，则读取目标文件1M字节，并将其写入文件；并打印出字节在文件的位置； 
-			{
-				fseek(f,-(1024*1024),SEEK_CUR);
-				fread(buffer,sizeof(buffer),1,f);
-				fwrite(buffer,sizeof(buffer),1,f1);
-				printf("i am here %d\n",ftell(f));
-				fclose(f1);	
-				last_f=ftell(f); 
-			}
-		}
-	}
-	printf("begin zonghe the file tfboy.mp3");	//开始合并文件； 
-	fclose(f);
-	FILE *f2=fopen("e:\\tfboyzonghe.mp3","wb");	//打开合并后目标文件数据流 
-	for (i=0;i<6;i++)
-		{
-			FILE *f1=fopen(a[i],"rb");
-			if (fread(buffer,sizeof(buffer),1,f1)!=0)	//如果成功读取到1M字节，将其写入zonghe文件里； 
-			{
-				fseek(f1,-(1024*1024),SEEK_CUR);
-				fread(buffer,sizeof(buffer),1,f1);
-				fwrite(buffer,sizeof(buffer),1,f2);
-				fclose(f1);
-			}
-			else 									//如果不成功，一个字节一个字节写入； 
-			{
-				fseek(f1,0,SEEK_SET);
-				printf("i am here%d\n",ftell(f1)); 
-				while(fread(one_buffer,sizeof(one_buffer),1,f1)!=0)
-				{
-					
-					fseek(f1,-1,SEEK_CUR);
-					fread(one_buffer,1,1,f1);
-					fwrite(one_buffer,1,1,f2);
-						
-				}
-				fclose(f1);		//关闭数据流 
-			}
-		}
-		fclose(f2);		//关闭数据流 
-} 
+	int i,j,num,v_start,flag=1;
+	int d[graphnum]={0};
+	int t[graphnum]={0};
+	int arcs[graphnum][graphnum]={0};
+	int parcs[graphnum][graphnum]={0};
+	void dijkstra(int a[graphnum][graphnum],int *d,int *t,int p[graphnum][graphnum]);
 
+	printf("input arch ifo\n");
+	while(flag)		//初始化arcs矩阵，即无向图的邻接矩阵 
+	{
+		scanf("%d%d%d",&i,&j,&num);
+		if(i<0||i>=graphnum||j<0||j>=graphnum||num<0)
+		{
+			printf("illegal input\n");
+			flag=0;
+		}
+		else
+		{
+			
+			arcs[i][j]=num;	
+			arcs[j][i]=num;
+		} 
+		
+	}	
+	for (i=0;i<graphnum;i++)	//打印arcs矩阵 
+	{
+		
+			for(j=0;j<graphnum;j++) 
+			{			
+				printf("%d\t",arcs[i][j]);
+				if(j==graphnum-1)
+				{
+					printf("\n");	
+				}				
+			}
+				
+	}
+	printf("input source v, value must from 0 to %d \n",graphnum-1);
+	for(v_start=0;v_start<graphnum;v_start++)	// 遍历图中的每个节点，求出某个节点到其他节点的最短路径和经过的点 
+	{
+		printf("this is about the v%d\n",v_start);
+		d[v_start]=0;
+		t[v_start]=1;
+		for (i=0;i<graphnum;i++)		//初始化d矩阵，用来记住最短路径的长度 
+		{
+			if(arcs[v_start][i]||i==v_start)		
+			{	
+				d[i]=arcs[v_start][i];		
+			}
+			else
+			{
+				d[i]=255;
+				
+			}
+			for(j=0;j<graphnum;j++) 	//初始化pracs矩阵用来记录最短路径
+			{			
+				parcs[i][j]=0;						
+			}
+			if(d[i]<255)
+			{
+				parcs[i][v_start]=1;
+				parcs[i][i]=1;			 
+			}
+					
+		}
+		dijkstra(arcs,d,t,parcs);
+		printf("this is shortest path length\n");
+		for (i=0;i<graphnum;i++)	//打印最短路径长度 
+		{					
+			printf("this the v%d:%d\t",i,d[i]);				
+		}
+		printf("\nthis is shortest path\n");
+		for (i=0;i<graphnum;i++)		//打印vx节点到其他节点最短路径经过的节点，第i行j列为1代表到第i个节点最短路径有经过j节点 ，反之则没有 
+		 
+		{					
+			for(j=0;j<graphnum;j++) 	
+			{			
+				printf("%d\t",parcs[i][j]);
+				if(j==graphnum-1)
+				{
+					printf("\n");	
+				}					
+			}
+							
+		}
+		for (i=0;i<graphnum;i++)
+		{
+			t[i]=0;
+		}
+	}
+	
+}
+void dijkstra(int a[graphnum][graphnum],int *d,int *t,int p[graphnum][graphnum])//利用dijkstra算法 
+{
+	int i,j,k,h;
+	int min=255;
+	int min_num;
+	for (i=1;i<graphnum;i++)
+	{
+		min=255;
+		for (j=1;j<graphnum;j++) 
+		{
+		  
+			 if(t[j]==0&&d[j]<min)
+			 {
+			 	min_num=j;
+				min=d[j];	
+			 }
+		}	
+		if(min_num>=0&&min_num<graphnum)
+		{
+		
+			t[min_num]=1;
+			for(k=0;k<graphnum;k++)
+			{
+				if(a[min_num][k])
+				{
+					if((a[min_num][k]+min<d[k])&&t[k]==0)
+					{
+						d[k]=a[min_num][k]+min;
+						for (h=0;h<graphnum;h++)
+						{
+							p[k][h]=p[min_num][h];
+						}
+						p[k][k]=1;
+					}
+				}
+			}
+		}
+	}
+}
